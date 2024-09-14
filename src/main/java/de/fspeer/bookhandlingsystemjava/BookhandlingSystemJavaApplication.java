1184b48206700b342dd6a0be09f6e691871bb54c
ferdinandspeer <ferdinandspeer@gmail.com>
2024-09-14 11:00:34 +0200

    Initial commit of self learning task

---
+++ src/main/java/de/fspeer/bookhandlingsystemjava/BookhandlingSystemJavaApplication.java
@@ -0,0 +1,13 @@
+package de.fspeer.bookhandlingsystemjava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookhandlingSystemJavaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BookhandlingSystemJavaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/de/fspeer/bookhandlingsystemjava/BookhandlingSystemJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package de.fspeer.bookhandlingsystemjava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookhandlingSystemJavaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
